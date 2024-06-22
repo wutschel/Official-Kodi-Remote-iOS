7f069ec2fa01c770dfcaec39350f419a99188250
wutschel <andreebuschmann@t-online.de>
2024-06-22 21:47:05 +0200

    Call reloadData directly instead via performSelectorOnMainThread
    We are on main thread, performSelectorOnMainThread is same as a direct call in this case.

--- XBMC Remote/NowPlaying.m
+++ XBMC Remote/NowPlaying.m
@@ -2252,10 +2252,10 @@
             else if (sourceIndexPath.row < storeSelection.row && destinationIndexPath.row >= storeSelection.row) {
                 storeSelection = [NSIndexPath indexPathForRow:storeSelection.row - 1 inSection:storeSelection.section];
             }
-            [playlistTableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:YES];
+            [playlistTableView reloadData];
         }
         else {
-            [playlistTableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:YES];
+            [playlistTableView reloadData];
             [playlistTableView selectRowAtIndexPath:storeSelection animated:YES scrollPosition:UITableViewScrollPositionMiddle];
         }
     }];
@@ -2284,7 +2284,7 @@
                 }
             }
             else {
-                [playlistTableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:YES];
+                [playlistTableView reloadData];
                 [playlistTableView selectRowAtIndexPath:storeSelection animated:YES scrollPosition:UITableViewScrollPositionMiddle];
             }
         }];
