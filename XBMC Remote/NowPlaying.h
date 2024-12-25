84405dcdece8710ef9a1cae2185113645671a55b
wutschel <andreebuschmann@t-online.de>
2024-12-25 18:11:10 +0100

    Introduce BaseActionViewController as base class
    This base class implements actions shared by multiple UIViewControllers, e.g. play, queue, open URL, show remote, simple JSON commands and more.

--- XBMC Remote/NowPlaying.h
+++ XBMC Remote/NowPlaying.h
@@ -11,12 +11,13 @@
 #import "UIImageView+WebCache.h"
 #import "RightMenuViewController.h"
 #import "OBSlider.h"
+#import "BaseActionViewController.h"
 
 @class ShowInfoViewController;
 @class RemoteController;
 @class DetailViewController;
 
-@interface NowPlaying : UIViewController <UITableViewDataSource, UITableViewDelegate, SDWebImageManagerDelegate, UIGestureRecognizerDelegate> {
+@interface NowPlaying : BaseActionViewController <UITableViewDataSource, UITableViewDelegate, SDWebImageManagerDelegate, UIGestureRecognizerDelegate> {
     IBOutlet UIView *transitionView;
     IBOutlet UITableView *playlistTableView;
     IBOutlet UILabel *albumName;
@@ -105,7 +106,6 @@
 - (IBAction)changeShuffle:(id)sender;
 - (IBAction)changeRepeat:(id)sender;
 
-@property (strong, nonatomic) id detailItem;
 @property (strong, nonatomic) RemoteController *remoteController;
 @property (strong, nonatomic) UIImageView *jewelView;
 @property (strong, nonatomic) IBOutlet UIImageView *itemLogoImage;
