d6d6d31427453a15420cb9efac56b43ae33a7147
wutschel <andreebuschmann@t-online.de>
2025-10-23 14:51:51 +0200

    Remove obsolete ivar audioVolume and method volumeInfo
    Both were only relevant to block sending volume up/down incase the volume was already at max/min.

--- XBMC Remote/RemoteController.m
+++ XBMC Remote/RemoteController.m
@@ -264,21 +264,16 @@
 }
 
 - (void)handleRotate:(id)sender {
-    if ([(UIRotationGestureRecognizer*)sender state] == UIGestureRecognizerStateBegan) {
-        [self volumeInfo];
-    }
-	else if ([(UIRotationGestureRecognizer*)sender state] == UIGestureRecognizerStateEnded) {
+    if ([(UIRotationGestureRecognizer*)sender state] == UIGestureRecognizerStateEnded) {
 		lastRotation = 0.0;
 		return;
 	}
 	CGFloat rotation = 0.0 - (lastRotation - [(UIRotationGestureRecognizer*)sender rotation]);
     
     if (rotation > ROTATION_TRIGGER) {
-        audioVolume = MIN(audioVolume + 1, 100);
         [self changeServerVolume:@"increment"];
     }
     else if (rotation < -ROTATION_TRIGGER) {
-        audioVolume = MAX(audioVolume - 1, 0);
         [self changeServerVolume:@"decrement"];
     }
 	lastRotation = [(UIRotationGestureRecognizer*)sender rotation];
@@ -508,15 +503,6 @@
             [Utilities sendXbmcHttp:callback];
         }
     }];
-}
-
-- (void)volumeInfo {
-    if (AppDelegate.instance.serverVolume > -1) {
-        audioVolume = AppDelegate.instance.serverVolume;
-    }
-    else {
-        audioVolume = 0;
-    }
 }
 
 - (void)changeServerVolume:(id)value {
@@ -1043,7 +1029,6 @@
         [self.navigationController setNavigationBarHidden:NO animated:YES];
     }
     quickHelpView.alpha = 0.0;
-    [self volumeInfo];
     
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(revealMenu:)
