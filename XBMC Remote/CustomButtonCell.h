5646d5bd64bc2b8f697b4d9fd2a67335d70af9de
wutschel <andreebuschmann@t-online.de>
2025-10-26 00:38:23 +0200

    fixup: property instead of ivar

--- XBMC Remote/CustomButtonCell.h
+++ XBMC Remote/CustomButtonCell.h
@@ -10,12 +10,7 @@
 
 #define CUSTOM_BUTTON_ITEM_HEIGHT 50.0
 
-@interface CustomButtonCell : UITableViewCell {
-    UILabel *buttonLabel;
-    UIImageView *buttonIcon;
-    UISwitch *onoffSwitch;
-    UIActivityIndicatorView *busyView;
-}
+@interface CustomButtonCell : UITableViewCell
 
 @property (nonatomic, strong) UILabel *buttonLabel;
 @property (nonatomic, strong) UIImageView *buttonIcon;
