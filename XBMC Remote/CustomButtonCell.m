2e7446c5d759cd8dad825aad9b90444a924d5d36
wutschel <andreebuschmann@t-online.de>
2025-10-26 01:43:52 +0200

    fixup: remove comments

--- XBMC Remote/CustomButtonCell.m
+++ XBMC Remote/CustomButtonCell.m
@@ -81,12 +81,10 @@
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated {
     [super setEditing:editing animated:animated];
     if (editing) {
-        // Disable the UISWitch and align the alpha of the icons
         self.onoffSwitch.enabled = NO;
         self.buttonIcon.alpha = 0.5;
     }
     else {
-        // Enable the UISWitch and align the alpha of the icons
         self.onoffSwitch.enabled = YES;
         self.buttonIcon.alpha = 0.9;
     }
