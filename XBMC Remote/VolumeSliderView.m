8a70b8d05a1c1e1c8385de5f65e93d15a6815294
wutschel <andreebuschmann@t-online.de>
2025-10-26 16:43:51 +0100

    Align naming of mute methods with volume methods
    - ServerMute instead of MuteServer or Mute
    - read = read from JSON API and show status
    - show = show status
    - change = write to JSON API

--- XBMC Remote/VolumeSliderView.m
+++ XBMC Remote/VolumeSliderView.m
@@ -135,7 +135,7 @@
         [plusButton setImage:img forState:UIControlStateNormal];
         [plusButton setImage:img forState:UIControlStateHighlighted];
         
-        [self checkMuteServer];
+        [self readServerMute];
         
         [self setVolumeButtonMode];
         
@@ -164,7 +164,7 @@
 
 - (void)handleServerStatusChanged:(NSNotification*)sender {
     [self showServerVolume];
-    [self checkMuteServer];
+    [self readServerMute];
 }
 
 - (void)handleApplicationOnVolumeChanged:(NSNotification*)sender {
@@ -266,11 +266,11 @@
 }
 
 - (IBAction)toggleMute:(id)sender {
-    [self handleMute:!isMuted];
-    [self changeMuteServer];
+    [self showServerMute:!isMuted];
+    [self changeServerMute];
 }
 
-- (void)handleMute:(BOOL)mute {
+- (void)showServerMute:(BOOL)mute {
     if (!AppDelegate.instance.serverOnLine) {
         return;
     }
@@ -290,13 +290,13 @@
     volumeSlider.userInteractionEnabled = !isMuted;
 }
 
-- (void)changeMuteServer {
+- (void)changeServerMute {
     [[Utilities getJsonRPC]
      callMethod:@"Application.SetMute"
      withParameters:@{@"mute": @"toggle"}];
 }
 
-- (void)checkMuteServer {
+- (void)readServerMute {
     [[Utilities getJsonRPC]
      callMethod:@"Application.GetProperties"
      withParameters:@{@"properties": @[@"muted"]}
@@ -304,7 +304,7 @@
      onCompletion:^(NSString *methodName, NSInteger callId, id methodResult, DSJSONRPCError *methodError, NSError *error) {
          if (error == nil && methodError == nil && [methodResult isKindOfClass:[NSDictionary class]]) {
              isMuted = [methodResult[@"muted"] boolValue];
-             [self handleMute:isMuted];
+             [self showServerMute:isMuted];
          }
     }];
 }
