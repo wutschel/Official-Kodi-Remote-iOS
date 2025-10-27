105091d5068fc38c59e0f4252bdf5db0c237534d
wutschel <andreebuschmann@t-online.de>
2025-10-27 20:21:00 +0100

    Add support to handle URLs when calling the app
    Implement application:openURL:options: and introduce a helper method to map the given name or host to the app's server list and connect to this server. In case there is no matching server in the list, no server will be connected and even the last connected server will be disconnected. This ensures the desired server is connected, no matter what.
    
    Change the URL scheme to "kodiremote", so that the remote app can be called via URL like "kodiremote://host" or "kodiremote://name".

--- XBMC Remote/AppDelegate.m
+++ XBMC Remote/AppDelegate.m
@@ -170,6 +170,46 @@
 
 #pragma mark - Helper
 
+- (BOOL)connectToServerFromList:(NSString*)host {
+    if (!host.length) {
+        return NO;
+    }
+    
+    // Host name needs ".local." at the end
+    if ([host hasSuffix:@".local"]) {
+        host = [host stringByAppendingString:@"."];
+    }
+    
+    // Try to map server name or IP address to the list of Kodi servers
+    NSInteger index = [self.arrayServerList indexOfObjectPassingTest:^BOOL(NSDictionary *obj, NSUInteger idx, BOOL *stop) {
+        return [host isEqualToString:obj[@"serverDescription"]] || [host isEqualToString:obj[@"serverIP"]];
+    }];
+    
+    // We want to connect to the desired server only. If this is not present, disconnect from any active server.
+    // Case 1: App just starts. Set the last active server before readKodiServerParameters is called
+    // Case 2: App already runs. Send a notification to select the server via its index.
+    if (index != NSNotFound) {
+        // Case 1
+        NSIndexPath *serverIndexPath = [NSIndexPath indexPathForRow:index inSection:0];
+        [Utilities saveLastServerIndex:serverIndexPath];
+        
+        // Case 2
+        NSDictionary *params = @{@"index": @(index)};
+        [NSNotificationCenter.defaultCenter postNotificationName:@"SelectKodiServer" object:nil userInfo:params];
+        
+        return YES;
+    }
+    else {
+        // Case 1
+        [Utilities saveLastServerIndex:nil];
+        
+        // Case 2
+        [NSNotificationCenter.defaultCenter postNotificationName:@"SelectKodiServer" object:nil userInfo:nil];
+        
+        return NO;
+    }
+}
+
 - (void)setIdleTimerFromUserDefaults {
     UIApplication.sharedApplication.idleTimerDisabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"lockscreen_preference"];
 }
@@ -264,6 +304,11 @@
             NSLog(@"CFSocketSendData error: %li", CFSocketSendData_error);
         }
     }
+}
+
+- (BOOL)application:(UIApplication*)app openURL:(NSURL*)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id>*)options {
+    // Use URL host to map to server list and connect the server.
+    return [self connectToServerFromList:url.host.stringByRemovingPercentEncoding];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication*)application {
