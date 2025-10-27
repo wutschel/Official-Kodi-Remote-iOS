65c4fec4c6f57fad10d13f487e6dec408538aedb
wutschel <andreebuschmann@t-online.de>
2025-10-27 13:09:02 +0100

    test

---
+++ XBMC Remote/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  Kodi Remote
+//
+//  Created by Buschmann on 27.10.25.
+//  Copyright © 2025 Team Kodi. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+
+#endif /* test_h */
---
+++ XBMC Remote/test.mm
@@ -0,0 +1,9 @@
+//
+//  test.mm
+//  Kodi Remote
+//
+//  Created by Buschmann on 27.10.25.
+//  Copyright © 2025 Team Kodi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
