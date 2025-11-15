96b48b67815074b88fb1b3b18716671111f4ffd1
wutschel <andreebuschmann@t-online.de>
2025-11-15 18:31:27 +0100

    Use UIView extensions

--- XBMC Remote/RemoteController.m
+++ XBMC Remote/RemoteController.m
@@ -121,10 +121,7 @@ static void *TorchRemoteContext = &TorchRemoteContext;
         remoteControlView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin;
         
         CGFloat topPadding = isEmbeddedMode ? [Utilities getTopPadding] : 0;
-        CGRect frame = volumeSliderView.frame;
-        frame.origin.y = topPadding;
-        frame.origin.x = leftPadding;
-        volumeSliderView.frame = frame;
+        [volumeSliderView setOrigin:CGPointMake(leftPadding, topPadding)];
     }
     else {
         // Used to avoid drawing remote buttons into the safe area
@@ -315,27 +312,15 @@ static void *TorchRemoteContext = &TorchRemoteContext;
 }
 
 - (void)setLayoutForGestureMode {
-    CGRect frame = gestureZoneView.frame;
-    frame.origin.x = 0;
-    gestureZoneView.frame = frame;
-    
-    frame = buttonZoneView.frame;
-    frame.origin.x = self.view.frame.size.width;
-    buttonZoneView.frame = frame;
-    
+    [gestureZoneView setX:0];
+    [buttonZoneView setX:self.view.frame.size.width];
     gestureZoneView.alpha = 1;
     buttonZoneView.alpha = 0;
 }
 
 - (void)setLayoutForButtonMode {
-    CGRect frame = gestureZoneView.frame;
-    frame.origin.x = -self.view.frame.size.width;
-    gestureZoneView.frame = frame;
-    
-    frame = buttonZoneView.frame;
-    frame.origin.x = 0;
-    buttonZoneView.frame = frame;
-    
+    [gestureZoneView setX:-self.view.frame.size.width];
+    [buttonZoneView setX:0];
     gestureZoneView.alpha = 0;
     buttonZoneView.alpha = 1;
 }
